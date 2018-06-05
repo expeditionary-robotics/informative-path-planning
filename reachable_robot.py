14f27b2527b0a87979142dabda391c7221b3cd2e
vpreston <victorialynnpreston@gmail.com>
2018-06-05 14:51:32 -0400

    Adding nonmyopic functionality

--- reachable_robot.py
+++ reachable_robot.py
@@ -35,6 +35,228 @@ MIN_COLOR = -25.
 MAX_COLOR = 25.
 
 
+class MCTS_Reachable:
+    '''Class that establishes a MCTS for nonmyopic planning'''
+
+    def __init__(self, computation_budget, belief, initial_pose, rollout_length, goal_selections, f_rew, time, ranges, aq_param = None, sample_step=0.5, step_size=1.5, turning_radius = 0.001):
+        '''Initialize with constraints for the planning, including whether there is a budget or planning horizon
+        Inputs:
+            computation_budget (float) number of seconds to run the tree building procedure
+            belief (GP model) current belief of the vehicle
+            initial_pose (tuple of floats) location of the vehicle in world coordinates
+            rollout_length (int) number of actions to rollout after selecting a child (tree depth)
+            frontier_size (int) number of options for each action in the tree (tree breadth)
+            path_generator (string) how action sets should be developed
+            aquisition_function (function) the criteria to make decisions
+            f_rew (string) the name of the function used to make decisions
+            time (float) time in the global world used for aquisition weighting
+        '''
+
+        # Parameterization for the search
+        self.comp_budget = computation_budget
+        self.GP = belief
+        self.cp = initial_pose
+        self.rl = rollout_length
+        self.goals = goal_selections
+        self.ranges = ranges
+        if f_rew == 'mes':
+            self.aquisition_function = il.mves
+        elif f_rew == 'mean':
+            self.aquisition_function = il.mean_UCB
+        elif f_rew == 'exp_improve':
+            self.aquisition_function = il.exp_improvement
+        else:
+            print 'ERROR!!!!'
+        self.f_rew = f_rew
+        self.t = time
+        self.turning_radius = turning_radius
+        self.step_size = step_size
+        self.sample_step = sample_step
+
+        # The tree
+        self.tree = None
+        
+        # Elements which are relevant for some acquisition functions
+        self.params = None
+        self.max_val = None
+        self.max_locs = None
+        self.current_max = aq_param
+
+    def take_step(self, loc, goal):
+        '''
+        Create an intermediary goal towards the point of interest such that the robot only translates the step size specified
+        Input: Goal
+        Output: Navigable points to the intermediary goal
+        '''
+        coords = {}
+
+        dist = np.sqrt((loc[0]-goal[0])**2 + (loc[1]-goal[1])**2)
+        angle_to_goal = np.arctan2([goal[1]-loc[1]], [goal[0]-loc[0]])[0]
+        if dist > self.step_size:
+            new_goal = (loc[0]+self.step_size*np.sin(np.pi/2-angle_to_goal), loc[1]+self.step_size*np.sin(angle_to_goal), angle_to_goal)
+        else:
+            new_goal = (goal[0], goal[1], angle_to_goal)
+
+        path = dubins.shortest_path(loc, new_goal, self.turning_radius)
+        configurations, _ = path.sample_many(self.sample_step)
+        configurations.append(new_goal)
+
+        temp = []
+        for i,config in enumerate(configurations):
+            if config[0] > self.ranges[0] and config[0] < self.ranges[1] and config[1] > self.ranges[2] and config[1] < self.ranges[3]:
+                temp.append(config)
+            else:
+                pass
+
+        return temp 
+
+    def choose_trajectory(self, t):
+        ''' Main function loop which makes the tree and selects the best child
+        Output:
+            path to take, cost of that path
+        '''
+        # initialize tree
+        self.tree = self.initialize_tree() 
+        i = 0 #iteration count
+
+        # randonly sample the world for entropy search function
+        if self.f_rew == 'mes':
+            self.max_val, self.max_locs = il.sample_max_vals(self.GP, t = t)
+            
+        time_start = time.clock()            
+            
+        # while we still have time to compute, generate the tree
+        while time.clock() - time_start < self.comp_budget:
+            i += 1
+            current_node = self.tree_policy()
+            sequence = self.rollout_policy(current_node)
+            reward = self.get_reward(sequence)
+            self.update_tree(reward, sequence)
+
+        # get the best action to take with most promising futures
+        best_sequence, best_val, all_vals = self.get_best_child()
+        print "Number of rollouts:", i, "\t Size of tree:", len(self.tree)
+        logger.info("Number of rollouts: {} \t Size of tree: {}".format(i, len(self.tree)))
+
+        # paths = self.path_generator.get_path_set(self.cp)                
+        return self.tree[best_sequence][0], best_val, self.goals, all_vals, self.max_locs, self.max_val
+
+    def initialize_tree(self):
+        '''Creates a tree instance, which is a dictionary, that keeps track of the nodes in the world
+        Output:
+            tree (dictionary) an initial tree
+        '''
+        tree = {}
+        # root of the tree is current location of the vehicle
+        tree['root'] = (self.cp, 0) #(pose, number of queries)
+        for i,goal in enumerate(self.goals):
+            samples = self.take_step(self.cp, goal)
+            cost = np.sqrt((self.cp[0]-samples[-1][0])**2 + (self.cp[1]-samples[-1][1])**2)
+            tree['child '+ str(i)] = (samples, cost, 0, 0) #(samples, cost, reward, number of times queried)
+        return tree
+
+    def tree_policy(self):
+        '''Implements the UCB policy to select the child to expand and forward simulate. From Arora paper, the following is defined:
+            avg_r - average reward of all rollouts that have passed through node n
+            c_p - some arbitrary constant, they use 0.1
+            N - number of times parent has been evaluated
+            n - number of times that node has been evaluated
+            the formula: avg_r + c_p * np.sqrt(2*np.log(N)/n)
+        '''
+        leaf_eval = {}
+        # TODO: check initialization, when everything is zero. appears to be throwing error
+        actions = self.goals
+        for i, val in enumerate(actions):
+            try:
+                node = 'child '+ str(i)
+                leaf_eval[node] = self.tree[node][2] + 0.1*np.sqrt(2*(np.log(self.tree['root'][1]))/self.tree[node][3])
+            except:
+                pass
+        return max(leaf_eval, key=leaf_eval.get)
+
+    def rollout_policy(self, node):
+        '''Select random actions to expand the child node
+        Input:
+            node (the name of the child node that is to be expanded)
+        Output:
+            sequence (list of names of nodes that make the sequence in the tree)
+        '''
+
+        sequence = [node] #include the child node
+        #TODO use the cost metric to signal action termination, for now using horizon
+        for i in xrange(self.rl):
+            actions = self.goals #self.path_generator.get_path_set(self.tree[node][0][-1]) #plan from the last point in the sample
+            try:
+                a = np.random.randint(0,len(actions)-1) #choose a random path
+            except:
+                if len(actions) != 0:
+                    a = 0
+
+            samples = self.take_step(self.tree[node][0][-1], actions[a])
+            self.tree[node + ' child ' + str(a)] = (samples, 0, 0, 0) #add random path to the tree
+            node = node + ' child ' + str(a)
+            sequence.append(node)
+
+        return sequence
+
+    def get_reward(self, sequence):
+        '''Evaluate the sequence to get the reward, defined by the percentage of entropy reduction.
+        Input:
+            sequence (list of strings) names of the nodes in the tree
+        Outut:
+            reward value from the aquisition function of choice
+        '''
+        sim_world = self.GP
+        samples = []
+        obs = []
+        for seq in sequence:
+            samples.append(self.tree[seq][0])
+        obs = list(chain.from_iterable(samples))
+
+        if self.f_rew == 'mes':
+            return self.aquisition_function(time = self.t, xvals = obs, robot_model = sim_world, param = self.max_val)
+        elif self.f_rew == 'exp_improve':
+            return self.aquisition_function(time=self.t, xvals = obs, robot_model = sim_world, param = [self.current_max])
+        else:
+            return self.aquisition_function(time=self.t, xvals = obs, robot_model = sim_world)
+
+    
+    def update_tree(self, reward, sequence):
+        '''Propogate the reward for the sequence
+        Input:
+            reward (float) the reward or utility value of the sequence
+            sequence (list of strings) the names of nodes that form the sequence
+        '''
+        #TODO update costs as well
+        self.tree['root'] = (self.tree['root'][0], self.tree['root'][1]+1)
+        for seq in sequence:
+            samples, cost, rew, queries = self.tree[seq]
+            queries += 1
+            n = queries
+            rew = ((n-1)*rew+reward)/n
+            self.tree[seq] = (samples, cost, rew, queries)
+
+    def get_best_child(self):
+        '''Query the tree for the best child in the actions
+        Output:
+            (string, float) node name of the best child, the cost of that child
+        '''
+        best = -float('inf')
+        best_child = None
+        value = {}
+        for i in xrange(len(self.goals)):
+            try:
+                r = self.tree['child '+ str(i)][2]
+                value[i] = r
+                #if r > best and len(self.tree['child '+ str(i)][0]) > 1: 
+                if r > best: 
+                    best = r
+                    best_child = 'child '+ str(i)
+            except:
+                pass
+        return best_child, best, value
+
+
 class Reachable_Robot():
     '''This robot inherits from the Robot class, but uses a MCTS in order to perform global horizon planning'''
     
@@ -66,9 +288,9 @@ class Reachable_Robot():
         elif f_rew == 'info_gain':
             self.aquisition_function = il.info_gain
         elif f_rew == 'mes':
-            self.aquisition_function = il.mves
+            self.aquisition_function = mves_2
         elif f_rew == 'exp_improve':
-            self.aquisition_function = il.exp_improvement
+            self.aquisition_function = exp_improvement_2
         else:
             raise ValueError('Only \'hotspot_info\' and \'mean\' and \'info_gain\' and \'mes\' and \'exp_improve\' reward fucntions supported.')
         
@@ -150,18 +372,19 @@ class Reachable_Robot():
         coords = {}
 
         dist = np.sqrt((self.loc[0]-goal[0])**2 + (self.loc[1]-goal[1])**2)
-        angle_to_goal = np.arcsin((goal[1]-self.loc[1])/(dist)) * np.sign(goal[0]-self.loc[0])
-        print 'here'
-        print self.loc
-        print 'goal'
-        print goal
+        angle_to_goal = np.arctan2([goal[1]-self.loc[1]], [goal[0]-self.loc[0]])[0]
+        # print 'here'
+        # print self.loc
+        # print 'goal'
+        # print goal
+        # print 'angle_to_goal'
+        # print angle_to_goal
         if dist > self.step_size:
             new_goal = (self.loc[0]+self.step_size*np.sin(np.pi/2-angle_to_goal), self.loc[1]+self.step_size*np.sin(angle_to_goal), angle_to_goal)
         else:
-            print 'too close'
             new_goal = (goal[0], goal[1], angle_to_goal)
-        print 'new_goal'
-        print new_goal
+        # print 'new_goal'
+        # print new_goal
 
 
         path = dubins.shortest_path(self.loc, new_goal, self.turning_radius)
@@ -171,17 +394,11 @@ class Reachable_Robot():
         temp = []
         for i,config in enumerate(configurations):
             if config[0] > self.ranges[0] and config[0] < self.ranges[1] and config[1] > self.ranges[2] and config[1] < self.ranges[3]:
-                # return config
                 temp.append(config)
             else:
-                print config
-                temp = []
+                pass
+                # temp = []
                 # break
-
-        # if len(temp) < 2:
-        #     pass
-        # else:
-        #     coords[i] = temp
 
         return temp 
 
@@ -224,8 +441,21 @@ class Reachable_Robot():
         for t in xrange(T):
             print "[", t, "] Current Location:  ", self.loc
             logger.info("[{}] Current Location: {}".format(t, self.loc))
-            best_location, best_val, all_locations, all_values, max_locs = self.choose_destination(t = t)
-            
+            if self.use_mcts == False:
+                best_location, best_val, all_locations, all_values, max_locs = self.choose_destination(t = t)
+                print best_location, best_val
+                sampling_path = self.take_step(best_location)
+            else:
+                if self.f_rew == "exp_improve":
+                    param = self.current_max
+                else:
+                    param = None
+
+                mcts = MCTS_Reachable(self.comp_budget, self.GP, self.loc, self.roll_length, self.goals, self.f_rew, t, aq_param = param, turning_radius=self.turning_radius, sample_step=self.sample_step, step_size = self.step_size, ranges=self.ranges)
+                best_path, best_val, all_paths, all_vals, max_locs, max_val = mcts.choose_trajectory(t = t)
+                sampling_path = best_path
+
+
             # Given this choice, update the evaluation metrics 
             pred_loc, pred_val = self.predict_max()
             print "Current predicted max and value: \t", pred_loc, "\t", pred_val
@@ -241,7 +471,6 @@ class Reachable_Robot():
 
 
             # Given this choice, take a step in the right direction, obeying to the dynamics of the vehicle
-            sampling_path = self.take_step(best_location)
             print sampling_path
             if len(sampling_path) == 0:
                 break
@@ -389,6 +618,65 @@ def mean_UCB_2(time, xvals, robot_model, param=None):
 
     return np.sum(mu) + np.sqrt(beta_t) * np.sum(np.fabs(var))
 
+def exp_improvement_2(time, xvals, robot_model, param = None):
+    ''' The aquisition function using expected information, as defined in Hennig and Schuler Entropy Search'''
+    data = xvals#np.array(xvals)
+    x1 = data[0]
+    x2 = data[1]
+    queries = np.vstack([x1,x2]).T
+
+    mu, var = robot_model.predict_value(queries)
+    avg_reward = 0
+
+    if param == None:
+        eta = 0.5
+    else:
+        eta = sum(param)/len(param)
+
+    # z = (np.sum(mu)-eta)/np.sum(np.fabs(var))
+    x = [m-eta for m in mu]
+    x = np.sum(x)
+    z = x/np.sum(np.fabs(var))
+    big_phi = 0.5 * (1 + sp.special.erf(z/np.sqrt(2)))
+    small_phi = 1/np.sqrt(2*np.pi) * np.exp(-z**2 / 2) 
+    avg_reward = x*big_phi + np.sum(np.fabs(var))*small_phi#(np.sum(mu)-eta)*big_phi + np.sum(np.fabs(var))*small_phi
+        
+    return avg_reward
+
+def mves_2(time, xvals, robot_model, param):
+    ''' Define the Acquisition Function and the Gradient of MES'''
+    # Compute the aquisition function value f and garident g at the queried point x using MES, given samples
+    # function maxes and a previous set of functino maxes
+    maxes = param
+    # If no max values are provided, return default value
+    if maxes is None:
+        return 1.0
+
+    data = xvals
+    x1 = data[0]
+    x2 = data[1]
+    queries = np.vstack([x1, x2]).T        
+    
+    d = queries.shape[1] # The dimension of the points (should be 2D)     
+
+    # Initialize f, g
+    f = 0
+    for i in xrange(maxes.shape[0]):
+        # Compute the posterior mean/variance predictions and gradients.
+        #[meanVector, varVector, meangrad, vargrad] = mean_var(x, xx, ...
+        #    yy, KernelMatrixInv{i}, l(i,:), sigma(i), sigma0(i));
+        mean, var = robot_model.predict_value(queries)
+        std_dev = np.sqrt(var)
+        
+        # Compute the acquisition function of MES.        
+        gamma = (maxes[i] - mean) / var
+        pdfgamma = sp.stats.norm.pdf(gamma)
+        cdfgamma = sp.stats.norm.cdf(gamma)
+        f += sum(gamma * pdfgamma / (2.0 * cdfgamma) - np.log(cdfgamma))        
+    # Average f
+    f = f / maxes.shape[0]
+    # f is an np array; return scalar value
+    return f[0]
 
 if __name__ == '__main__':
     seed = 0#int(sys.argv[1])
@@ -429,9 +717,13 @@ if __name__ == '__main__':
                   sample_step = 0.5,
                   evaluation = evaluation, 
                   f_rew = reward_function, 
-                  create_animation = True) 
+                  create_animation = True,
+                  discretization=(20,20),
+                  use_mcts = True,
+                  computation_budget = 10.0,
+                  rollout_length = 5) 
 
-    robot.planner(T = 30)
+    robot.planner(T = 10)
     #robot.visualize_world_model(screen = True)
     robot.visualize_trajectory(screen = False)
     # robot.plot_information()
