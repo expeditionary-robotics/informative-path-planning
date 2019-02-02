ed7b9a1347d99726d15818952e7516ae08ceee73
vpreston <victorialynnpreston@gmail.com>
2019-02-02 11:52:44 -0500

    Adding backwards and forwards action

--- composit_planner/src/path_fabricator.py
+++ composit_planner/src/path_fabricator.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python
 
 '''
-This library can be used to access the multiple ways in which path sets can be generated for the simulated vehicle in the PLUMES framework.
+This library can be used to access the multiple ways in which path sets can be generated for
+the simulated vehicle in the PLUMES framework.
 
 License: MIT
 Maintainers: Genevieve Flaspohler and Victoria Preston
 '''
 
-import numpy as np
-import math
+import copy
 import dubins
+import numpy as np
 import rospy
 from geometry_msgs.msg import *
-from nav_msgs.msg import * 
+from nav_msgs.msg import *
 from sensor_msgs.msg import *
 from std_msgs.msg import *
 from composit_planner.srv import *
 from composit_planner.msg import *
-from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from tf import TransformListener
-import copy
 
-class ROS_Path_Generator():
+
+class ROS_Path_Generator(object):
+    ''' Uses dubins library to generate valid, safe paths for the vehicle.
+    '''
     def __init__(self):
         ''' Initialize a path generator
         Input:
-            frontier_size (int) the number of points on the frontier we should consider for navigation
+            frontier_size (int) the number of points on the frontier to consider for navigation
             horizon_length (float) distance between the vehicle and the horizon to consider
             turning_radius (float) the feasible turning radius for the vehicle
             sample_step (float) the unit length along the path from which to draw a sample
@@ -33,22 +35,24 @@ class ROS_Path_Generator():
         rospy.init_node("path_generator")
 
         # the parameters for the dubin trajectory
-        self.fs = rospy.get_param('frontier_size',15)
-        self.hl = rospy.get_param('horizon_length',1.5)
-        self.tr = rospy.get_param('turning_radius',0.05)
-        self.ss = rospy.get_param('sample_step',0.5)
-        self.ang = rospy.get_param('frontier_angle_range',np.pi/4)
+        self.fs = rospy.get_param('frontier_size', 15)
+        self.hl = rospy.get_param('horizon_length', 1.5)
+        self.tr = rospy.get_param('turning_radius', 0.05)
+        self.ss = rospy.get_param('sample_step', 0.5)
+        self.ang = rospy.get_param('frontier_angle_range', np.pi/4)
         self.safe_threshold = rospy.get_param('cost_limit', 50.)
-        self.unknown_threshold = rospy.get_param('unknown_limit',-2.0)
-        self.make_paths_behind = rospy.get_param('make_paths_behind',False)
+        self.unknown_threshold = rospy.get_param('unknown_limit', -2.0)
+        self.make_paths_behind = rospy.get_param('make_paths_behind', False)
         self.make_stay_path = rospy.get_param('allow_to_stay', True)
+        self.visualize_paths = rospy.get_param('visualize_path_generation', True)
+
         # Global variables
         self.goals = [] #The frontier coordinates
         self.samples = {} #The sample points which form the paths
-        self.cp = (0,0,0) #The current pose of the vehicle
+        self.cp = (0, 0, 0) #The current pose of the vehicle
 
+        # Services and Publishers
         self.srv_path = rospy.Service('get_paths', PathFromPose, self.get_path_set)
-
         self.cost_srv = rospy.ServiceProxy('obstacle_map', GetCostMap)
         self.path_pub = rospy.Publisher('/path_options', PointCloud, queue_size=1)
         self.tf_listener = TransformListener()
@@ -58,20 +62,13 @@ class ROS_Path_Generator():
 
     def generate_frontier_points(self):
         '''From the frontier_size and horizon_length, generate the frontier points to goal'''
-        angle = np.linspace(-self.ang,self.ang,self.fs) #fix the possibilities to 75% of the unit circle, ignoring points directly behind the vehicle
+        angle = np.linspace(-self.ang, self.ang, self.fs)
         goals = []
         for a in angle:
-            x = self.hl*np.cos(self.cp[2]+a)+self.cp[0]
-            y = self.hl*np.sin(self.cp[2]+a)+self.cp[1]
-            p = self.cp[2]+a
-            goals.append((x,y,p))
+            goals.append(trig_projection(self.cp, self.hl, a))
 
-            if self.make_paths_behind == True:
-                a = np.unwrap([a+np.pi])[0]
-                x = self.hl*np.cos(self.cp[2]+a)+self.cp[0]
-                y = self.hl*np.sin(self.cp[2]+a)+self.cp[1]
-                p = self.cp[2]+a
-                goals.append((x,y,p))
+        #also append a path directly in front of the vehicle
+        goals.append(trig_projection(self.cp, self.hl, 0))
 
         self.goals = goals
         return self.goals
@@ -80,33 +77,34 @@ class ROS_Path_Generator():
         '''Connect the current_pose to the goal places'''
         all_paths = []
         for goal in self.goals:
-            # if np.arctan2(goal[1]-self.cp[1],goal[0]-self.cp[0]) > 0:
-            ang = np.arctan2(goal[1]-self.cp[1],goal[0]-self.cp[0])
-            relative_angle = np.fabs(np.unwrap([ang])[0])
             paths = dubins.shortest_path(self.cp, goal, self.tr)
-            '''
-            if relative_angle < np.pi/2 or relative_angle > 3*np.pi/2:
-                paths = dubins.shortest_path(self.cp, goal, self.tr)
-            else:
-                adjusted = np.unwrap([self.cp[2]-np.pi])[0]
-                cp = (self.cp[0],self.cp[1],adjusted)
-                paths = dubins.shortest_path(cp, goal, self.tr)
-            '''
             true_path, _ = paths.sample_many(self.ss)
             all_paths.append(true_path)
 
-        if self.make_stay_path == True:
-            all_paths.append([self.cp for i in range(0,int(self.hl/self.ss))])
+        if self.make_paths_behind is True:
+            # make a single path directly behind the vehicle
+            behind_goal = trig_projection(self.cp, self.hl, 3*np.pi/2)
+            paths = dubins.shortest_path((self.cp[0], self.cp[1], self.cp[2]+3*np.pi/2),
+                                         behind_goal,
+                                         self.tr)
+            true_path, _ = paths.sample_many(self.ss)
+            all_paths.append(true_path)
+
+        if self.make_stay_path is True:
+            all_paths.append([self.cp for i in range(0, int(self.hl/self.ss)+1)])
 
         return all_paths
 
     def rosify_safe_path(self, paths):
+        '''
+        Transforms relative coordinates to rosmessage types and checks that they are safe
+        '''
         clear_paths = []
 
         # Get the costmap
         map_resp = self.cost_srv(GetCostMapRequest())
         current_map = map_resp.map
-        data = self.make_array(current_map.data, current_map.info.height, current_map.info.width)
+        data = make_array(current_map.data, current_map.info.height, current_map.info.width)
 
         # Check the poses
         self.viz = []
@@ -115,18 +113,18 @@ class ROS_Path_Generator():
             idx = [int(round((x[1]-current_map.info.origin.position.y)/current_map.info.resolution)) for x in path]
 
             try:
-                cost_vals = data[idx,idy]
-                cost = np.sum([k for k in cost_vals if k>=0.])
-                unknown_cost = np.sum([k for k in cost_vals if k<0.])
+                cost_vals = data[idx, idy]
+                cost = np.sum([k for k in cost_vals if k >= 0.])
+                unknown_cost = np.sum([k for k in cost_vals if k < 0.])
             except:
                 cost = 0
                 unknown_cost = 0
                 for m, n in zip(idx, idy):
                     try:
-                        if data[m,n] >= 0:
-                            cost += data[m,n]
+                        if data[m, n] >= 0:
+                            cost += data[m, n]
                         else:
-                            unknown_cost += data[m,n]
+                            unknown_cost += data[m, n]
 
                     except:
                         break
@@ -134,21 +132,24 @@ class ROS_Path_Generator():
 
             if (cost < self.safe_threshold and unknown_cost > self.unknown_threshold) and len(path) > 0:
                 clear_paths.append(self.make_rosmsg(path))
-        
+
         # Make a debugging message
-        m = PointCloud()
-        m.header.frame_id = 'world'
-        m.header.stamp = rospy.Time(0)
-        m.points = self.viz
-        val = ChannelFloat32()
-        val.name = 'path_options'
-        val.values = np.ones(np.size(self.viz))
-        m.channels.append(val)
-        self.path_pub.publish(m)
+        if self.visualize_paths is True:
+            m = PointCloud()
+            m.header.frame_id = 'world'
+            m.header.stamp = rospy.Time(0)
+            m.points = self.viz
+            val = ChannelFloat32()
+            val.name = 'path_options'
+            val.values = np.ones(np.size(self.viz))
+            m.channels.append(val)
+            self.path_pub.publish(m)
         # Return all of the polygons to assess, includes header information
         return clear_paths
 
-    def make_rosmsg(self,path):
+    def make_rosmsg(self, path):
+        ''' Does the ROS message conversion
+        '''
         pub_path = []
         for coord in path:
             pc = Point32()
@@ -167,7 +168,8 @@ class ROS_Path_Generator():
     def get_path_set(self, req):
         '''Primary interface for getting list of path sample points for evaluation
         Input:
-            current_pose (tuple of x, y, z, a which are floats) current location of the robot in world coordinates
+            current_pose (tuple of x, y, z, a which are floats) current location of the robot in
+            world coordinates
         Output:
             paths (dictionary of frontier keys and sample points)
         '''
@@ -186,18 +188,16 @@ class ROS_Path_Generator():
         self.map_frame = (msg.x, msg.y, msg.z)
         return self.map_frame #for checking obstacles
 
-    def make_array(self,data,height,width):
-        return np.array(data).reshape((height,width),order='C')#self.make_array(msg.data, msg.info.height, msg.info.width)
+def make_array(data, height, width):
+    ''' Converts occupancy grid vector to useable array'''
+    return np.array(data).reshape((height, width), order='C')
 
-        # output = np.zeros((height,width))
-        # for i in range(width):
-        #     for j in range(height):
-        #         output[i,j] = data[i+j*width]
-        # return output
-
-    def extractz(self, l):
-        l.z=0
-        return l
+def trig_projection(point, step_size, angle):
+    ''' Function to perform a path projection'''
+    x = step_size*np.cos(point[2]+angle)+point[0]
+    y = step_size*np.sin(point[2]+angle)+point[1]
+    p = point[2]+angle
+    return (x, y, p)
 
 
 if __name__ == '__main__':
--- composit_planner/src/traj_serv.py
+++ composit_planner/src/traj_serv.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
 
-# Copyright 2018 Massachusetts Institute of Technology
+'''Copyright 2018 Massachusetts Institute of Technology'''
 
 import rospy
 import numpy as np
-import actionlib
 from composit_planner.srv import *
 from composit_planner.msg import *
-from nav_msgs.msg import Path
 from geometry_msgs.msg import *
-from tf.transformations import quaternion_from_euler
 
-#TODO this node can be used to trigger replanning in the midst of executing a trajectory already
-
-class TrajMonitor():
+class TrajMonitor(object):
+    '''This class handles the replan trigger event based on the location of the vehicle relative to
+    the last valid goal point.
+    '''
     def __init__(self):
-        #initialize node and callback
+        '''initialize node and callback'''
         rospy.init_node('execute_dubin')
 
-        self.allowed_error = rospy.get_param('trajectory_endpoint_precision',0.2)
-        self.replan_distance = None #TODO use this to trigger replan
+        self.allowed_error = rospy.get_param('trajectory_endpoint_precision', 0.2)
         self.last_viable = None
+        self.new_goals = None
 
         #subscribe to trajectory topic
         self.sub = rospy.Subscriber("/trajectory/current", PolygonStamped, self.handle_trajectory, queue_size=1)
         self.pose_sub = rospy.Subscriber("/pose", PoseStamped, self.handle_pose, queue_size=1)
+
         #access replan service to trigger when finished a trajectory
         self.replan = rospy.ServiceProxy('replan', RequestReplan)
 
@@ -34,10 +33,9 @@ class TrajMonitor():
 
     def handle_trajectory(self, traj):
         '''
-        The trajectory comes in as a series of poses. It is assumed that the desired angle has already been determined
+        The trajectory comes in as a series of poses. It is assumed that the desired angle has
+        already been determined
         '''
-        # print 'Getting planning point'
-
         check_points = traj.polygon.points
         if self.last_viable != -1:
             if self.last_viable is not None and np.isclose(check_points[-1].x, self.last_viable.x) is True:
@@ -50,15 +48,14 @@ class TrajMonitor():
 
 
     def handle_pose(self, msg):
+        '''
+        Listens for the robot's current pose and triggers replan event
+        '''
         last = self.last_viable
         if last is not None:
             if last != -1:
-                # print "CHECKING POSE NOW"
-                # print last
-                # print msg.pose.position
                 if (msg.pose.position.x-last.x)**2 + (msg.pose.position.y-last.y)**2 < self.allowed_error**2:
-                    # self.last_viable = -1
-                    print "~~~~~~~~TRIGGERING REPLAN~~~~~~~~~~~~"
+                    # print "~~~~~~~~TRIGGERING REPLAN~~~~~~~~~~~~"
                     self.replan()
             else:
                 pass
